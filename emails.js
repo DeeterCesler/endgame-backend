56634dade2da0942a016123847cae7f706a35fa0
Deeter <deeter.cesler@gmail.com>
2020-05-09 13:39:43 -0600

    remove print statements

--- emails.js
+++ emails.js
@@ -30,11 +30,11 @@ const sender = (template, userEmail) => {
         // html body
         });
     
-        console.log("Message sent: %s", info.messageId);
+        // console.log("Message sent: %s", info.messageId);
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
     
         // Preview only available when sending through an Ethereal account
-        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     }
     
@@ -42,7 +42,7 @@ const sender = (template, userEmail) => {
 }
 
 const sendResetPasswordEmail = (foundUser) => {
-    console.log('found user: ' + foundUser);
+    // console.log('found user: ' + foundUser);
     const userEmail = foundUser.email;
     const userName = foundUser.name;
     const userId = foundUser._id;
